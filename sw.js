330a18f55b826947dfdc00bc93765028fd758d63
hohoham <hasugu1220@gmail.com>
2025-09-09 16:20:30 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'japanese-review-v1';
+const urlsToCache = [
+  '/japanese_review/',
+  '/japanese_review/static/js/bundle.js',
+  '/japanese_review/static/css/main.css',
+  '/japanese_review/manifest.json',
+  '/japanese_review/icon-192x192.png',
+  '/japanese_review/icon-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached response if found
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
